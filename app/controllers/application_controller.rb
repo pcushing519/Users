8d5e247438c8bdffde366cc006c9380fcad23540
Patty Cushing <pcushing@latinschool.org>
2014-07-22 16:00:56 +0000

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,14 @@ class ApplicationController < ActionController::Base
      @user=User.find_by_id(params['id'])
   end
   
+  def new
+  end
   
-  
+  def create
+    u = User.new
+    u.name = params['name']
+    u.url = params['email']
+    u.save
+    redirect_to "/users/#{ u.id }"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
   get '/users/:id' => 'application#show'
-
+  get '/new_user' => 'application#new'
+  get '/create_user' => 'application#create'
 end
